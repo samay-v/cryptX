18f5f7309c21b041a777befb04d357a4b438a3b9
samay-v <samay1231998@gmail.com>
2019-07-25 00:01:06 +0530

    change port to env variable

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.use(authenticationRoutes);
 app.use(portfolioRoutes);
 app.use(investmentRoutes);
 
-app.listen("3000", () => {
-    console.log("server is working for cryptx on port 3000");
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log("server is working for cryptX on port: ", PORT);
 });
